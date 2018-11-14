aea227d27294bccb20cf371e2038e8f98ca7902b
Madenityan <marinakorsyn@gmail.com>
2018-11-14 22:20:13 +0200

    added saving and loading list

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 function newElement() {
     let item = document.getElementById('task').value;
-    let toDoList = JSON.parse(localStorage.getItem('task')) || {};
-
     let text = document.createTextNode(item);
     let newItem = document.createElement('li');
     newItem.appendChild(text);
     addIcon(newItem);
     document.getElementById('toDoList').appendChild(newItem);
 
+    let toDoList = JSON.parse(localStorage.getItem('task')) || {};
     toDoList[Object.keys(toDoList).length]={item};
     localStorage.setItem('task', JSON.stringify(toDoList));
+
     document.forms[0].reset();
+
 
     if (item === '' || null) {
         alert('Add some task!');
         newItem.style.display = "none";
     }
 }
+
+function onLoad() {
+    let toDoList = JSON.parse(localStorage.getItem('task')) || {};
+    for (let i = 0; i < Object.keys(toDoList).length; i++) {
+        let text = toDoList[i].item;
+        let newLi = document.createElement('li');
+        let newText = document.createTextNode(text);
+        newLi.appendChild(newText);
+        addIcon(newLi);
+        document.getElementById('toDoList').appendChild(newLi);
+    }
+}
+
 
 function addIcon(element) {
     let span = document.createElement("SPAN");
@@ -30,3 +44,7 @@ function addIcon(element) {
 function removeItem(event) {
     event.target.parentElement.remove();
 }
+
+window.onload = ( )=> {
+    onLoad();
+};
