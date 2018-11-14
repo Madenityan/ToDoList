e5dc8550cc06b5cbe4f0e114c41d42718455d427
Madenityan <marinakorsyn@gmail.com>
2018-11-14 16:11:48 +0200

    added reset for input and recorded element values in localStorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 function newElement() {
     let item = document.getElementById('task').value;
+    let toDoList = JSON.parse(localStorage.getItem('task')) || {};
+
     let text = document.createTextNode(item);
     let newItem = document.createElement('li');
     newItem.appendChild(text);
     addIcon(newItem);
     document.getElementById('toDoList').appendChild(newItem);
-    localStorage.setItem('task', item);
-}
 
+    toDoList[Object.keys(toDoList).length]={item};
+    localStorage.setItem('task', JSON.stringify(toDoList));
+    document.forms[0].reset();
+
+    if (item === '' || null) {
+        alert('Add some task!');
+        newItem.style.display = "none";
+    }
+}
 
 function addIcon(element) {
     let span = document.createElement("SPAN");
